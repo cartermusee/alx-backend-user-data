8257d6e58dfddc8bd5a3d1fd78373450d6d03134
macilah-carter <museemacilah@gmail.com>
2024-02-13 12:40:47 +0300

    files

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """class for auth methods"""
     def require_auth(self, path: str,
                      excluded_paths: List[str]) -> bool:
         """ public method for require auth method
