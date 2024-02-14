5071d1dc3b893b79ee987eb685a1fb7b2b1e4d5b
macilah-carter <museemacilah@gmail.com>
2024-02-14 21:39:38 +0300

    session and cokies

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,9 @@ class SessionAuth(Auth):
             session_id = str(uuid.uuid4())
             self.user_id_by_session_id.update({session_id: user_id})
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """method that returns a User ID based on a Session ID:"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        self.user_id_by_session_id.get(session_id)
