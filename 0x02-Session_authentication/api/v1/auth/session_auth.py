4401fe614e9f1a3a1544fd83eb1fcc1fc6895402
macilah-carter <museemacilah@gmail.com>
2024-02-14 21:18:25 +0300

    session and cokies

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,9 +4,17 @@ from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 import base64
+import uuid
 from models.user import User
 
 
 class SessionAuth(Auth):
     """clas session auth"""
-    pass
+    user_id_by_session_id = []
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+        return session_id
