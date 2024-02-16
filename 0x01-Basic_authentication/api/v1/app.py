70ed1112c5706802e647c3fe7404fbf8e5244e32
macilah-carter <museemacilah@gmail.com>
2024-02-16 14:48:14 +0300

    basic session auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,13 +17,12 @@ auth = None
 auth_type = getenv('AUTH_TYPE')
 
 
-if auth:
-    if auth_type == 'auth':
-        from api.v1.auth.auth import Auth
-        auth = Auth()
-    elif auth_type == 'basic_auth':
-        from api.v1.auth.basic_auth import BasicAuth
-        auth = BasicAuth()
+if auth_type == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
