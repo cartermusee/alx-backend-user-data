2fbbc56bf7e6eed33ae29fcd72c20ec8b3f720d5
macilah-carter <museemacilah@gmail.com>
2024-02-16 15:32:01 +0300

    basic session auth session

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -18,12 +18,14 @@ def login():
         return jsonify({"error": "email missing"}), 400
     if not pasword:
         return jsonify({"error": "password missing"}), 400
+
     user = User.search({'email': email})
     if user is None:
         return jsonify({"error": "no user found for this email"}), 404
     if not user.is_valid_password(pasword):
         return jsonify({"error": "wrong password"}), 401
 
+    from api.v1.app import auth
     session_id = auth.create_session(user.id)
 
     ses_name = os.getenv('SESSION_NAME', '_my_session_id')
