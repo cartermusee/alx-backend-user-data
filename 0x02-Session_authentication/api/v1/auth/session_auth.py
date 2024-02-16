b2fac6db1df446e93b29e628b61252bcde103948
macilah-carter <museemacilah@gmail.com>
2024-02-16 15:44:49 +0300

    basic session auth session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -53,6 +53,5 @@ class SessionAuth(Auth):
         sess_id = self.user_id_for_session_id(cookie_val)
         if not sess_id:
             return False
-        if sess_id in self.user_id_by_session_id:
-            del self.user_id_by_session_id[cookie_val]
+        del self.user_id_by_session_id[cookie_val]
         return True
