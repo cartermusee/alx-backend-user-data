357d9f3df37e5815f2be4dd40227c00577aa549b
macilah-carter <museemacilah@gmail.com>
2024-02-16 14:50:14 +0300

    basic session auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -40,8 +40,8 @@ def before():
         pass
     if auth.authorization_header(request) is None:
         abort(401)
-    request.current_user = auth.current_user(request)
-    if auth.current_user(request) is None:
+    user = auth.current_user(request)
+    if user is None:
         abort(403)
 
 
