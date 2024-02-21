e3df56b42fcd0c78b8644f04080721116c6c31a4
macilah-carter <museemacilah@gmail.com>
2024-02-21 15:40:43 +0300

    made executable app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,5 +66,16 @@ def profile():
         abort(403)
 
 
+@app.route('reset_password', methods=['POST'])
+def get_reset_password_token():
+    """reset password"""
+    email = request.form.get("email")
+    if not email:
+        abort(403)
+    else:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
