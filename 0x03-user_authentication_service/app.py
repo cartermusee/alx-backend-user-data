2d08378bc900ef7fbb1c6e1cd26490657579a2f5
macilah-carter <museemacilah@gmail.com>
2024-02-21 13:19:35 +0300

    made executable app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """module for a flask app"""
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
 AUTH = Auth()
@@ -40,6 +40,18 @@ def login() -> str:
         return res
     else:
         abort(401)
+
+
+@app.route("/sessions", methods=['DELETE'])
+def logout():
+    """logout method"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
